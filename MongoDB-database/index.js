5bc4a860154c5580633bea314f5d5409e002ede5
Nagarjun k <53399371+nagarjunkumar@users.noreply.github.com>
2021-06-16 21:03:15 +0530

    Mongodb schema and model defined

--- MongoDB-database/index.js
+++ MongoDB-database/index.js
@@ -5,12 +5,14 @@ const mongoose = require("mongoose");
 
 dotenv.config({ path: "./../config.env" }); // Create a file called config.env and specify database url string
 
+// Read environment variables
 DATABASE_LOCAL = process.env.DATABASE_LOCAL;
 DATABASE_REMOTE = process.env.DATABASE_REMOTE;
 const PORT = 3000;
 
 const DB = DATABASE_REMOTE;
 
+// Connect to remote hosted mongodb cluster
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -24,6 +26,25 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
+
+// Define Schema for database
+const bookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A book name is required"],
+  },
+  rating: {
+    type: float,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+});
+
+// Create a model
+const Books = mongoose.model("Books", bookSchema);
 
 app = express();
 
