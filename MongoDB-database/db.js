493dcc6b117974eb222e07fc508e17313df10bb1
Nagarjun k <53399371+nagarjunkumar@users.noreply.github.com>
2021-06-17 16:24:21 +0530

    Documents uploading to db

---
+++ MongoDB-database/db.js
@@ -0,0 +1,46 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./../config.env" }); // Create a file called config.env and specify database url string
+
+// Read environment variables
+DATABASE_LOCAL = process.env.DATABASE_LOCAL;
+DATABASE_REMOTE = process.env.DATABASE_REMOTE;
+
+const DB = DATABASE_REMOTE;
+
+// Connect to remote hosted mongodb cluster
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log("Successfully connected to mongodb cluster");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+// Define Schema for database
+const bookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A book name is required"],
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+});
+
+// Create a model
+const Books = mongoose.model("Books", bookSchema);
+
+module.exports = Books;
--- MongoDB-database/index.js
+++ MongoDB-database/index.js
@@ -1,57 +1,37 @@
 const express = require("express");
 const morgan = require("morgan");
+const Books = require("./db");
+
+const PORT = 3000;
+
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 dotenv.config({ path: "./../config.env" }); // Create a file called config.env and specify database url string
 
-// Read environment variables
-DATABASE_LOCAL = process.env.DATABASE_LOCAL;
-DATABASE_REMOTE = process.env.DATABASE_REMOTE;
-const PORT = 3000;
-
-const DB = DATABASE_REMOTE;
-
-// Connect to remote hosted mongodb cluster
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then((conn) => {
-    console.log("Database Connection success full");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-
-// Define Schema for database
-const bookSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A book name is required"],
-  },
-  rating: {
-    type: float,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-});
-
-// Create a model
-const Books = mongoose.model("Books", bookSchema);
-
 app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Successfully connect to remote mongodb database");
+  const { name, rating, description } = req.query;
+  const input = new Books({
+    name: name,
+    rating: rating,
+    description: description,
+  });
+
+  input
+    .save()
+    .then((doc) => {
+      console.log("Inserted document", doc._id);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  console.log(name, rating, description);
+  res.send(name, rating, description);
 });
 
 app.listen(PORT, console.log(`Express running on ${PORT}..`));
